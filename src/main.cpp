86f944916520f21a3fefe1372a05d1514eb0b22a
PK-cod3ch3mist <pratyushkumartheking@gmail.com>
2021-01-09 13:53:17 +0530

    Changes to be committed: 	renamed:    fonts/.DS_Store -> assets/.DS_Store 	renamed:    fonts/Audiowide/Audiowide-Regular.ttf -> assets/fonts/Audiowide/Audiowide-Regular.ttf 	renamed:    fonts/Audiowide/OFL.txt -> assets/fonts/Audiowide/OFL.txt 	deleted:    bin/main.o 	renamed:    bin/sfml-app -> bin/minegui 	new file:   include/game.h 	new file:   src/Makefile 	new file:   src/game.cpp 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,67 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include "game.h"
 
-int main()
-{
-    // Create and open a window [used a constructor here]
-    // The class is sf::Window and object is window
-    sf::RenderWindow window(sf::VideoMode(400, 400), "SFML works!");
+using namespace sf;
 
-    sf::Font font;
-    if (!font.loadFromFile("../fonts/Audiowide/Audiowide-Regular.ttf"))
+// Defining some global objects which are essential to the program
+// For example, the main window
+// And the associated stuff
+RenderWindow window(VideoMode(400, 400), "Minesweeper v2.0");
+
+Font font;
+
+void init() {
+    if (!font.loadFromFile("../assets/fonts/Audiowide/Audiowide-Regular.ttf"))
     {
         std::cout << "Symbol font not loaded" << std::endl;
     }
+}
 
-    sf::Text text("Mine Sweeper v2.0", font, 30);
+void welcome(const Text &text, const RectangleShape &play_button, const RectangleShape &score_button, const Text &play_text, const Text &score_text, const Text &credit_text) {
+    window.draw(text);
+    window.draw(play_button);
+    window.draw(score_button);
+    window.draw(play_text);
+    window.draw(score_text);
+    window.draw(credit_text);
+}
+
+int main()
+{
+    init();
+
+    // Setting up the splash screen/welcome window
+    // Start here ...
+    Text text("Mine Sweeper v2.0", font, 30);
     text.setFillColor(sf::Color::White);
-    text.setPosition(40.f, 10.f);
+    text.setPosition(35.f, 10.f);
 
-    sf::RectangleShape play_button(sf::Vector2f(120.f, 50.f));
+    RectangleShape play_button(sf::Vector2f(120.f, 50.f));
     play_button.setPosition(140.f, 100.f);
 
-    sf::RectangleShape score_button(sf::Vector2f(160.f, 50.f));
+    RectangleShape score_button(sf::Vector2f(160.f, 50.f));
     score_button.setPosition(120.f, 200.f);
 
-    sf::Text play_text("PLAY", font, 30);
+    Text play_text("PLAY", font, 30);
     play_text.setFillColor(sf::Color::Black);
     play_text.setPosition(155.f, 105.f);
 
-    sf::Text score_text("SCORES", font, 30);
+    Text score_text("SCORES", font, 30);
     score_text.setFillColor(sf::Color::Black);
     score_text.setPosition(130.f, 205.f);
 
-    sf::Text credit_text("created by Ord-Com. MIT license\n see GitHub page for more info", font, 10);
+    Text credit_text("created by Ord-Com. MIT license\n see GitHub page for more info", font, 10);
     credit_text.setFillColor(sf::Color::White);
     credit_text.setPosition(100.f, 350.f);
+    // End here...
+    // The above are definitions required for the welcome window
 
     // Run program as long as the window is open
     while (window.isOpen())
     {
         // Check all the windows events triggered since the last iteration of the loop
-        sf::Event event;
+        Event event;
 
         // We use pollEvent which returns true if an event was pending, otherwise false
         while (window.pollEvent(event))
@@ -60,12 +82,7 @@ int main()
         }
 
         window.clear();
-        window.draw(text);
-        window.draw(play_button);
-        window.draw(play_text);
-        window.draw(score_button);
-        window.draw(score_text);
-        window.draw(credit_text);
+        welcome(text, play_button, score_button, play_text, score_text, credit_text);
         window.display();
     }
 
